7f4e46255954b02009035cb41a62783f6fba3201
JEON <jwodn123@naver.com>
2023-05-21 11:20:48 +0900

    fix : Modify Content CRUD junit5 test
    junti5, JPA 사용한 테스트.

--- src/main/java/com/example/tdd/web/repository/ContentRepository.java
+++ src/main/java/com/example/tdd/web/repository/ContentRepository.java
@@ -2,37 +2,7 @@ package com.example.tdd.web.repository;
 
 import com.example.tdd.web.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public interface ContentRepository extends JpaRepository<Content, Long> {
 
-@Repository
-public class ContentRepository {
-
-    private static Map<Integer, Content> contents = new HashMap<>();
-    private static int sequence = 0;
-
-    public void save(Content content) {
-        content.setId(++sequence);
-        contents.put(content.getId(), content);
-    }
-
-    public void edit(int id, Content content) {
-        contents.put(id, content);
-    }
-
-    public void delete(int id) {
-        contents.remove(id);
-    }
-
-    public List<Content> findAll() {
-        return new ArrayList<>(contents.values());
-    }
-
-    public Content findById(int id) {
-        return contents.get(id);
-    }
 }
